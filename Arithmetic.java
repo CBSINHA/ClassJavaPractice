03503fc83996dfc510d811ae65f9c40670acda71
Shashank Sinha <shashanksinha590@gmail.com>
2025-01-17 13:33:17 +0530

    Arithmetic operations

---
+++ Arithmetic.java
@@ -0,0 +1,10 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a=15;
+        int b=10;
+        int add=a+b;
+        int sub=a-b;
+        int mul=a*b;
+        System.out.println("Addition: "+add+"\nSubstraction: "+sub+"\nMultiplication: "+mul);
+    }
+}
