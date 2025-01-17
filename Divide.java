a89a0486aa352f72609576392931aff44a33b5fc
Shashank Sinha <shashanksinha590@gmail.com>
2025-01-17 13:50:10 +0530

    Divide

---
+++ Divide.java
@@ -0,0 +1,9 @@
+public class Divide {
+    public static void main(String[] args) {
+        int a=25;
+        float b=10;
+        //int c=a/b; //Will have to implement explicit type casting
+        float c=a/b;
+        System.out.println("Result: "+c);
+    }
+}
