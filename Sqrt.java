99361bfa33ce1179e7e49ebc9cb95bfdbffcb267
Shashank Sinha <shashanksinha590@gmail.com>
2025-01-17 13:27:13 +0530

    root

---
+++ Sqrt.java
@@ -0,0 +1,7 @@
+public class Sqrt {
+    public static void main(String[] ar){
+        double number=5;
+        double root=Math.sqrt(number);
+        System.out.println("The square root of 5 is: "+root);
+    }
+}
