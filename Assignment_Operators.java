33cf4510b12a8d80cdb3a1c4ec07fb9d2dc0b4d5
Shashank Sinha <shashanksinha590@gmail.com>
2025-01-27 16:13:28 +0530

    Add Assignment_Operators class to demonstrate assignment operations in Java

---
+++ Assignment_Operators.java
@@ -0,0 +1,12 @@
+public class Assignment_Operators {
+    public static void main(String[] args) {
+        int n1=10,n2=20,n3;
+        System.out.println(n3=n1);
+        System.out.println(n2+=n1);
+        System.out.println(n2-=n1);
+        System.out.println(n2*=n1);
+        System.out.println(n2/=n1);
+        System.out.println(n2^=n1);
+        System.out.println(n2%=n1);
+    }
+}
