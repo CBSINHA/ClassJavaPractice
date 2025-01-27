ac23d18c45b8a9fd5ac27b14c3109d211a64b40e
Shashank Sinha <shashanksinha590@gmail.com>
2025-01-27 15:19:49 +0530

    Add Wrapper class with main method for basic data type demonstration

---
+++ Wrapper.java
@@ -0,0 +1,10 @@
+class Wrapper{
+    public static void main(String[] args) {
+        Integer a=5;
+        Double b=7.7;
+        Character c='A';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
