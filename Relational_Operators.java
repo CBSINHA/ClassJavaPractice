25c214cc0161dc65c6a49aa28135a87a1ad6c75a
Shashank Sinha <shashanksinha590@gmail.com>
2025-01-27 15:54:19 +0530

    Add Relational_Operators class to demonstrate relational operations in Java

---
+++ Relational_Operators.java
@@ -0,0 +1,10 @@
+public class Relational_Operators {
+    public static void main(String[] args) {
+        int n2=20,n1=10;
+        System.out.println(n1<n2);
+        System.out.println(n1>n2);
+        System.out.println(n1<=n2);
+        System.out.println(n1>=n2);
+        System.out.println(n1!=n2);
+    }
+}
