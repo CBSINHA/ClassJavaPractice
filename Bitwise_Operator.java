c951e3da0b642aa16d647de555e52761f1be3f54
Shashank Sinha <shashanksinha590@gmail.com>
2025-01-27 16:21:54 +0530

    Add Bitwise_Operator class to demonstrate bitwise operations in Java

---
+++ Bitwise_Operator.java
@@ -0,0 +1,10 @@
+public class Bitwise_Operator {
+    public static void main(String[] args) {
+        int n1=58;
+        int n2=13;
+        System.out.println(n1&n2);
+        System.out.println(n1|n2);
+        System.out.println(n1^n2);
+        System.out.println(~n1);
+    }
+}
