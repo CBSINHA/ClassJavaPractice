cfb6fa3163c8d8566a31205b80a08431f946fd13
Shashank Sinha <shashanksinha590@gmail.com>
2025-01-27 15:50:51 +0530

    Add Logical_Operators class to demonstrate logical operations in Java

---
+++ Logical_Operators.java
@@ -0,0 +1,8 @@
+public class Logical_Operators {
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(n<5 &&n<20);
+        System.out.println(n<5||n<20);
+        System.out.println(!(n<5&&n<20));
+    }
+}
