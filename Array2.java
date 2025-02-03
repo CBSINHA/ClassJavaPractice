dcc332aad4af5e1d904d83136f48a3c4d33d53f5
Shashank Sinha <shashanksinha590@gmail.com>
2025-02-03 15:25:06 +0530

    implemented array in another way in java

---
+++ Array2.java
@@ -0,0 +1,8 @@
+public class Array2 {
+    public static void main(String[] args) {
+        int[]a={1,2,3,4,5,6,7,8,9,0};
+        for(int i:a){
+            System.out.print(i+" ");
+        }
+    }
+}
