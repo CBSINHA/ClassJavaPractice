45ccdc5f1a29934ea0f07ca56d138a7e175c9542
Shashank Sinha <shashanksinha590@gmail.com>
2025-02-07 12:19:35 +0530

    enum declaration

---
+++ Enums.java
@@ -0,0 +1,9 @@
+public class Enums {
+    enum Colors{
+        RED,GREEN,YELLOW,BLUE
+    }
+    public static void main(String args[]){
+        Colors colour=Colors.BLUE;
+        System.out.println(colour);
+    }
+}
