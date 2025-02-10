7081b0e97b2f106361b7068117feba6dc6cab266
Shashank Sinha <shashanksinha590@gmail.com>
2025-02-10 16:15:26 +0530

    Static method with parameters

---
+++ Static_MethodwithParameters.java
@@ -0,0 +1,6 @@
+public class Static_MethodwithParameters {
+    static void method(String str,int age){System.out.println("Static method got "+str+age);}
+    public static void main(String[] args) {
+        method("Shashank",111);
+    }
+}
