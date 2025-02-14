4ebb3ecba5153afae51b6fe5c1bb5782bb48a392
Shashank Sinha <shashanksinha590@gmail.com>
2025-02-14 13:28:02 +0530

    updated String Builder

--- Stringbuilder_class.java
+++ Stringbuilder_class.java
@@ -3,8 +3,10 @@ public class Stringbuilder_class {
         StringBuilder sb=new StringBuilder("Hello");
         System.out.println(sb.length());
         System.out.println(sb.reverse());
+        sb.reverse();
         int a=123;
         sb.append(a);
+        sb.insert(5,"XD");
         System.out.println(sb.toString());
     }
 }
