e78e7dc7cde99e15ef3876033a62ec7d8d465766
Shashank Sinha <shashanksinha590@gmail.com>
2025-02-14 13:19:13 +0530

    Built in string methods

---
+++ StringProgram.java
@@ -0,0 +1,8 @@
+public class StringProgram {
+    public static void main(String[] args) {
+        String str="Hello Badmos";
+        System.out.println("Length of string: "+str.length());
+        System.out.println("Is equal to another string ?: "+str.equals("Hello badmos"));
+        System.out.println("Using concat function: "+str.concat(" Shashank"));
+    }
+}
