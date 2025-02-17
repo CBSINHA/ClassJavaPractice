ed0304117cdd02d0156df682a766d9dec8d161f9
Shashank Sinha <shashanksinha590@gmail.com>
2025-02-17 15:41:02 +0530

    Updated inheritance

--- Inheritance.java
+++ Inheritance.java
@@ -9,10 +9,14 @@ class Animal{
 }
 class Dog extends Animal{void bark(){System.out.println("Bhau bhau bhau......");}}
 class Pilla extends Dog{void weep(){System.out.println("Kutte ka pilla ro raha hai T_T...");}}
+class Billa extends Animal{void meow(){System.out.println("Miau....");}}
 class Inheritance{
     public static void main(String[] args) {
         Beta obj=new Beta();
         Pilla pilla=new Pilla();
+        Billa billa=new Billa();
+        billa.eat();
+        billa.meow();
         System.out.println("Pitaji ki age: "+obj.age+ " Beta ji ki age: "+obj.age1);
         pilla.eat();
         pilla.bark();
