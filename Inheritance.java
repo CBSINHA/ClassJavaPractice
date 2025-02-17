f489455a58d30f6c4493424d108de26e0353a004
Shashank Sinha <shashanksinha590@gmail.com>
2025-02-17 15:29:38 +0530

    added member functions and used inheritance

--- Inheritance.java
+++ Inheritance.java
@@ -4,9 +4,16 @@ class Papa{
 class Beta extends Papa{
     int age1=20;
 }
+class Animal{
+    void eat(){System.out.println("eating.......");}
+}
+class Dog extends Animal{void bark(){System.out.println("Bhau bhau bhau......");}}
 class Inheritance{
     public static void main(String[] args) {
         Beta obj=new Beta();
+        Dog dog=new Dog();
         System.out.println("Pitaji ki age: "+obj.age+ " Beta ji ki age: "+obj.age1);
+        dog.eat();
+        dog.bark();
     }
 }
