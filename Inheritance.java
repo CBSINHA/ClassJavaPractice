d8f682d2547d6510dc2ff2a7152d8f72b3930ea6
Shashank Sinha <shashanksinha590@gmail.com>
2025-02-17 15:33:45 +0530

    Added multilevel inheritance

--- Inheritance.java
+++ Inheritance.java
@@ -8,12 +8,14 @@ class Animal{
     void eat(){System.out.println("eating.......");}
 }
 class Dog extends Animal{void bark(){System.out.println("Bhau bhau bhau......");}}
+class Pilla extends Dog{void weep(){System.out.println("Kutte ka pilla ro raha hai T_T...");}}
 class Inheritance{
     public static void main(String[] args) {
         Beta obj=new Beta();
-        Dog dog=new Dog();
+        Pilla pilla=new Pilla();
         System.out.println("Pitaji ki age: "+obj.age+ " Beta ji ki age: "+obj.age1);
-        dog.eat();
-        dog.bark();
+        pilla.eat();
+        pilla.bark();
+        pilla.weep();
     }
 }
