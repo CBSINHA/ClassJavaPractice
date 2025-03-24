0f206737575488dbe9461f27ac44be31817aace5
Shashank Sinha <shashanksinha590@gmail.com>
2025-03-24 16:33:28 +0530

    Add ExceptionHandling class to demonstrate try-catch for array index out of bounds

---
+++ ExceptionHandling.java
@@ -0,0 +1,11 @@
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        try{
+            int[]n={1,2,3};
+            System.out.println(n[3]);
+        }
+        catch(Exception e){
+            System.out.println("Out of bounds element accessed");
+        }
+    }
+}
