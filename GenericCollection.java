8fdfac50545f6820ce004f92ef7ad9539ab9193d
Shashank Sinha <shashanksinha590@gmail.com>
2025-04-18 13:01:59 +0530

    updated

--- GenericCollection.java
+++ GenericCollection.java
@@ -2,10 +2,18 @@ import java.util.*;
 class GenericCollection{
     public static void main(String[]ar){
         ArrayList <String> names=new ArrayList<>();
+        ArrayList<Integer> sum=new ArrayList<>();
+        sum.add(10);
+        sum.add(20);
+        System.out.println(sum.get(0)+sum.get(1)+"\n");
         names.add("Shashank");
         names.add("Sinha");
         System.out.println(names);
         names.set(0,"One");
         System.out.println(names.get(0));
+        System.out.println("\n");
+        for(int i:sum){
+            System.out.println(i);
+        }
     }
 }
