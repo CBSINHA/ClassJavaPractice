a5b22d93d0f48d571282fc858bdd86ab84a437df
Shashank Sinha <shashanksinha590@gmail.com>
2025-04-28 16:31:49 +0530

    sum

---
+++ Func2804.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Func2804{
+    static int sum(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int total=sum(10,10);
+        System.out.println(total);
+    }
+}
